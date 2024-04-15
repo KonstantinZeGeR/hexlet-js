c3e7b84d851d3d1066f4b5c2d734fc11f453e1d2
KonstantinZeGeR <zegelr23@gmail.com>
2024-04-15 22:15:53 +0400

    Refactor: перенос логики и обновление путей

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import _ from "lodash";
+import { getLastElement } from './src/index';
+     
+getLastElement();  // Вызов функции
+
 
 console.log("Hello, Hexlet!");
 console.log(_.last(["one", "two"]));
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import _ from "lodash";
+
+// Функция для получения последнего элемента массива
+export const getLastElement = (array) => {
+  return _.last(array);
+};
